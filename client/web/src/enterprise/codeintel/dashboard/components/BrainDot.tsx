c80c1ad233a58a82335078c112b2b6221aff724d
Eric Fritz <eric@eric-fritz.com>
2023-06-06 18:17:58 -0500

    Fix lint.

--- client/web/src/enterprise/codeintel/dashboard/components/BrainDot.tsx
+++ client/web/src/enterprise/codeintel/dashboard/components/BrainDot.tsx
@@ -11,14 +11,12 @@ export interface BrainDotProps {
     repoName: string
 }
 
-export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName }) => {
-    return (
-        <Tooltip content="View code intelligence summary">
-            <Link to={`/${repoName}/-/code-graph/dashboard`}>
-                <Button className={classNames('text-decoration-none', styles.braindot)} aria-label="Code graph">
-                    <Icon aria-hidden={true} svgPath={mdiBrain} />
-                </Button>
-            </Link>
-        </Tooltip>
-    )
-}
+export const BrainDot: React.FunctionComponent<BrainDotProps> = ({ repoName }) => (
+    <Tooltip content="View code intelligence summary">
+        <Link to={`/${repoName}/-/code-graph/dashboard`}>
+            <Button className={classNames('text-decoration-none', styles.braindot)} aria-label="Code graph">
+                <Icon aria-hidden={true} svgPath={mdiBrain} />
+            </Button>
+        </Link>
+    </Tooltip>
+)
