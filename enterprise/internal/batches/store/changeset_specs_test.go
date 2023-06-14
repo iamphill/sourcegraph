484ae934a655711af42ddef6a6ec35b6ba78f95d
Becca Steinbrecher <becca.steinbrecher@sourcegraph.com>
2023-06-14 17:02:33 -0600

    Add unsigned commit test

--- enterprise/internal/batches/store/changeset_specs_test.go
+++ enterprise/internal/batches/store/changeset_specs_test.go
@@ -350,7 +350,7 @@ func testStoreChangesetSpecs(t *testing.T, ctx context.Context, s *Store, clock 
 		}
 	})
 
-	t.Run("UpdateChangesetSpecCommitVerification", func(t *testing.T) {
+	t.Run("UpdateChangesetSpecCommitVerification with a signed commit", func(t *testing.T) {
 		for _, c := range changesetSpecs {
 			commit := github.RestCommit{
 				URL:     "https://api.github.com/repos/Birth-control-tech/birth-control-tech-BE/git/commits/dabd9bb07fdb5b580f168e942f2160b1719fc98f",
@@ -361,6 +361,34 @@ func testStoreChangesetSpecs(t *testing.T, ctx context.Context, s *Store, clock 
 					Verified:  true,
 					Reason:    "valid",
 					Signature: "*********",
+				},
+			}
+			specID := 9223372036854775807
+
+			want := c.Clone()
+			if err := s.UpdateChangesetSpecCommitVerification(ctx, int64(specID), &commit); err != nil {
+				t.Fatal(err)
+			}
+			have, err := s.GetChangesetSpecByID(ctx, int64(specID))
+			if err != nil {
+				t.Fatal(err)
+			}
+			if diff := cmp.Diff(have, want); diff != "" {
+				t.Fatal(diff)
+			}
+		}
+	})
+
+	t.Run("UpdateChangesetSpecCommitVerification with an unsigned commit", func(t *testing.T) {
+		for _, c := range changesetSpecs {
+			commit := github.RestCommit{
+				URL:     "https://api.github.com/repos/Birth-control-tech/birth-control-tech-BE/git/commits/dabd9bb07fdb5b580f168e942f2160b1719fc98f",
+				SHA:     "dabd9bb07fdb5b580f168e942f2160b1719fc98f",
+				NodeID:  "C_kwDOEW0OxtoAKGRhYmQ5YmIwN2ZkYjViNTgwZjE2OGU5NDJmMjE2MGIxNzE5ZmM5OGY",
+				Message: "Append Hello World to all README.md files",
+				Verification: github.Verification{
+					Verified: false,
+					Reason:   "unsigned",
 				},
 			}
 			specID := 9223372036854775807
