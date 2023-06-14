14af46b3c0e1c04e66b47063e54fc0803d745031
Tom Ross <tom@umpox.com>
2023-06-14 12:48:26 +0100

    more

--- client/cody/src/main.ts
+++ client/cody/src/main.ts
@@ -3,6 +3,7 @@ import * as vscode from 'vscode'
 import { RecipeID } from '@sourcegraph/cody-shared/src/chat/recipes/recipe'
 import { ConfigurationWithAccessToken } from '@sourcegraph/cody-shared/src/configuration'
 
+import { showAccountSwitcher } from './account-switcher'
 import { ChatViewProvider } from './chat/ChatViewProvider'
 import { DOTCOM_URL, LOCAL_APP_URL, isLoggedIn } from './chat/protocol'
 import { getAuthStatus } from './chat/utils'
@@ -194,9 +195,7 @@ const register = async (
             vscode.commands.executeCommand('workbench.action.openSettings', { query: 'Cody:' })
         ),
         vscode.commands.registerCommand('cody.history', () => chatProvider.setWebviewView('history')),
-        vscode.commands.registerCommand('cody.switch-account', () =>
-            vscode.commands.executeCommand('cody.status-bar.interacted')
-        ),
+        vscode.commands.registerCommand('cody.switch-account', () => showAccountSwitcher()),
         vscode.commands.registerCommand('cody.walkthrough.showLogin', () =>
             vscode.commands.executeCommand('workbench.view.extension.cody')
         ),
