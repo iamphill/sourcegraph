72672b4007f7ba91a000b4bb8bc88369a098ffc7
Beyang Liu <beyang@sourcegraph.com>
2023-06-14 11:25:34 -0700

    .

--- client/cody/src/completions/provider2.ts
+++ client/cody/src/completions/provider2.ts
@@ -1,1 +1,148 @@
-export class NewCompletionProvider implements CompletionProvider {}
+import { Message } from '@sourcegraph/cody-shared/src/sourcegraph-api'
+import { SourcegraphNodeCompletionsClient } from '@sourcegraph/cody-shared/src/sourcegraph-api/completions/nodeClient'
+
+import { Completion } from '.'
+import { ReferenceSnippet } from './context'
+import { messagesToText } from './prompts'
+import { AbstractCompletionProvider, CompletionProvider } from './provider'
+
+const COMPLETIONS_PREAMBLE = `You are Cody, a code completion AI developed by Sourcegraph.`
+
+export class NewCompletionProvider implements CompletionProvider {
+    constructor(
+        protected completionsClient: SourcegraphNodeCompletionsClient,
+        protected promptChars: number,
+        protected responseTokens: number,
+        protected snippets: ReferenceSnippet[],
+        protected prefix: string,
+        protected suffix: string,
+        protected injectPrefix: string,
+        protected languageId: string,
+        protected defaultN: number = 1
+    ) {}
+
+    // Returns the content specific prompt excluding additional referenceSnippets
+    createPromptPrefix(): Message[] {
+        // TODO(beyang): escape 'Human:' and 'Assistant:'
+        const prefixLines = this.prefix.split('\n')
+        if (prefixLines.length === 0) {
+            throw new Error('no prefix lines')
+        }
+
+        let prefixMessages: Message[]
+        if (prefixLines.length > 2) {
+            const endLine = Math.max(Math.floor(prefixLines.length / 2), prefixLines.length - 5)
+            prefixMessages = [
+                {
+                    speaker: 'human',
+                    text: COMPLETIONS_PREAMBLE,
+                },
+                {
+                    speaker: 'assistant',
+                    text: 'I am Cody, a code completion AI developed by Sourcegraph.',
+                },
+                {
+                    speaker: 'human',
+                    text:
+                        'Complete the following file:\n' +
+                        '```' +
+                        `\n${prefixLines.slice(0, endLine).join('\n')}\n` +
+                        '```',
+                },
+                {
+                    speaker: 'assistant',
+                    text: `\`\`\`\n${prefixLines.slice(endLine).join('\n')}${this.injectPrefix}`,
+                },
+            ]
+        } else {
+            prefixMessages = [
+                {
+                    speaker: 'human',
+                    text: 'Write some code',
+                },
+                {
+                    speaker: 'assistant',
+                    text: `Here is some code:\n\`\`\`\n${this.prefix}${this.injectPrefix}`,
+                },
+            ]
+        }
+
+        return prefixMessages
+    }
+
+    public emptyPromptLength(): number {
+        const promptNoSnippets = messagesToText(this.createPromptPrefix())
+        return promptNoSnippets.length - 10 // extra 10 chars of buffer cuz who knows
+    }
+
+    // Creates the resulting prompt and adds as many snippets from the reference
+    // list as possible.
+    protected createPrompt(): Message[] {
+        const prefixMessages = this.createPromptPrefix()
+        const referenceSnippetMessages: Message[] = []
+
+        let remainingChars = this.promptChars - this.emptyPromptLength()
+
+        if (this.suffix.length > 0) {
+            let suffix = ''
+            // We throw away the first 5 lines of the suffix to avoid the LLM to
+            // just continue the completion by appending the suffix.
+            const suffixLines = this.suffix.split('\n')
+            if (suffixLines.length > 5) {
+                suffix = suffixLines.slice(5).join('\n')
+            }
+
+            if (suffix.length > 0) {
+                const suffixContext: Message[] = [
+                    {
+                        speaker: 'human',
+                        text:
+                            'Add the following code snippet to your knowledge base:\n' +
+                            '```' +
+                            `\n${suffix}\n` +
+                            '```',
+                    },
+                    {
+                        speaker: 'assistant',
+                        text: '```\n// Ok```',
+                    },
+                ]
+
+                const numSnippetChars = messagesToText(suffixContext).length + 1
+                if (numSnippetChars <= remainingChars) {
+                    referenceSnippetMessages.push(...suffixContext)
+                    remainingChars -= numSnippetChars
+                }
+            }
+        }
+
+        for (const snippet of this.snippets) {
+            const snippetMessages: Message[] = [
+                {
+                    speaker: 'human',
+                    text:
+                        `Add the following code snippet (from file ${snippet.filename}) to your knowledge base:\n` +
+                        '```' +
+                        `\n${snippet.text}\n` +
+                        '```',
+                },
+                {
+                    speaker: 'assistant',
+                    text: 'Okay, I have added it to my knowledge base.',
+                },
+            ]
+            const numSnippetChars = messagesToText(snippetMessages).length + 1
+            if (numSnippetChars > remainingChars) {
+                break
+            }
+            referenceSnippetMessages.push(...snippetMessages)
+            remainingChars -= numSnippetChars
+        }
+
+        return [...referenceSnippetMessages, ...prefixMessages]
+    }
+
+    async generateCompletions(abortSignal: AbortSignal, n?: number): Promise<Completion[]> {
+        return []
+    }
+}
